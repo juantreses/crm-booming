ef497773067efd8b39130ee56891c22513c4af99
Server user <slimfi@dedi6186.your-server.de>
2025-12-20 15:49:44 +0100

    changes fileds bodyscan

--- custom/Espo/Modules/Vanko/Controllers/VankoApi.php
+++ custom/Espo/Modules/Vanko/Controllers/VankoApi.php
@@ -62,6 +62,12 @@ class VankoApi extends RecordBase
 
     public function postActionKickstart($params, $data)
     {
+$logData = [
+            'timestamp' => date('Y-m-d H:i:s'),
+            'params' => $params,
+            'data' => json_encode($data),
+        ];
+        file_put_contents('test.txt', print_r($logData, true) . "\n\n", FILE_APPEND);
         try {
             if (!$data) {
                 throw new BadRequest('No data provided');
