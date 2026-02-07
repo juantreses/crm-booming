95a79bbceb3d8f84f122e558422d12ca19b5128e
khaccount <booming@dedi6186.your-server.de>
2026-02-07 13:15:19 +0100

    Adds Google integration

---
+++ custom/Espo/Modules/Google/Hooks/Meeting/Google.php
@@ -0,0 +1,75 @@
+<?php
+/***********************************************************************************
+ * The contents of this file are subject to the Extension License Agreement
+ * ("Agreement") which can be viewed at
+ * https://www.espocrm.com/extension-license-agreement/.
+ * By copying, installing downloading, or using this file, You have unconditionally
+ * agreed to the terms and conditions of the Agreement, and You may not use this
+ * file except in compliance with the Agreement. Under the terms of the Agreement,
+ * You shall not license, sublicense, sell, resell, rent, lease, lend, distribute,
+ * redistribute, market, publish, commercialize, or otherwise transfer rights or
+ * usage to the software or any modified version or derivative work of the software
+ * created by or for you.
+ *
+ * Copyright (C) 2015-2026 EspoCRM, Inc.
+ *
+ * License ID: 7c9d34c02c060e385617e86b0bb9a65b
+ ************************************************************************************/
+
+namespace Espo\Modules\Google\Hooks\Meeting;
+
+use Espo\ORM\Entity;
+use Espo\ORM\EntityManager;
+
+class Google
+{
+    public static $order = 9;
+
+    private EntityManager $entityManager;
+
+    public function __construct(
+        EntityManager $entityManager
+    ) {
+        $this->entityManager = $entityManager;
+    }
+
+    public function beforeSave(Entity $entity): void
+    {
+        if (
+            !$entity->isNew() &&
+            $entity->isAttributeChanged('assignedUserId') &&
+            $entity->get('googleCalendarEventId')
+        ) {
+            $dummy = $this->entityManager->getNewEntity($entity->getEntityType());
+
+            $copyList = [
+                'name',
+                'assignedUserId',
+                'googleCalendarId',
+                'googleCalendarEventId',
+                'dateStart',
+                'dateEnd',
+            ];
+
+            foreach ($copyList as $field) {
+                $dummy->set($field, $entity->getFetched($field));
+            }
+
+            $dummy->set('deleted', true);
+
+            // @todo Revise.
+            $this->entityManager->saveEntity($dummy, ['skipHooks' => true]);
+            $this->entityManager->removeEntity($dummy, ['skipHooks' => true]);
+
+            $entity->set('googleCalendarEventId', '');
+            $entity->set('googleCalendarId', '');
+        }
+
+        if (!$entity->isNew() && $entity->getFetched('googleCalendarEventId') == 'FAIL') {
+            $entity->set('googleCalendarEventId', '');
+        }
+
+        // If the user was removed from attendees, the event still stays in the user's the Google calendar.
+        // @todo Remove such events from the Google calendar. Only for events originated from Espo.
+    }
+}
