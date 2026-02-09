942d2329421dc49cf2bdf4d9aab6512e23f9ec50
khaccount <booming@dedi6186.your-server.de>
2026-02-09 21:01:23 +0100

    Minor changes

---
+++ data/state.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  'cacheTimestamp' => 1770662709,
+  'microtimeState' => 1770662709.16335,
+  'appTimestamp' => 1770659311,
+  'currencyRates' => [
+    'EUR' => 1.0
+  ],
+  'version' => '9.3.0'
+];
