367f7e7acf4532cd8a3b5082eed240011a358bcc
khaccount <booming@dedi6186.your-server.de>
2026-02-10 17:11:26 +0100

    Resolve conflict and keep local state.php

---
+++ data/state.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  'cacheTimestamp' => 1770738438,
+  'microtimeState' => 1770738438.346106,
+  'appTimestamp' => 1770659311,
+  'currencyRates' => [
+    'EUR' => 1.0
+  ],
+  'version' => '9.3.0'
+];
