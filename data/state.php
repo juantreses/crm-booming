5600fc859e05ca492e94edc32b4e077be50706ed
khaccount <booming@dedi6186.your-server.de>
2026-02-10 05:54:30 +0100

    Ignores state

--- data/state.php
+++ data/state.php
@@ -1,7 +1,7 @@
 <?php
 return [
-  'cacheTimestamp' => 1770662709,
-  'microtimeState' => 1770662709.16335,
+  'cacheTimestamp' => 1770678122,
+  'microtimeState' => 1770678122.581654,
   'appTimestamp' => 1770659311,
   'currencyRates' => [
     'EUR' => 1.0
