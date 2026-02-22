577c198ef4932b84280557cf9c35bb07a174ae1c
juantreses <jandries@live.be>
2026-02-22 20:49:56 +0100

    Fixes group assignment on meetings

---
+++ custom/Espo/Modules/Booking/Hooks/Meeting/GroupAssignment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Espo\Modules\Booking\Hooks\Meeting;
+
+use Espo\Core\Hook\Hook\BeforeSave;
+use Espo\Custom\Services\GroupAssignmentService;
+use Espo\ORM\Entity;
+use Espo\ORM\EntityManager;
+use Espo\ORM\Repository\Option\SaveOptions;
+
+class GroupAssignment implements BeforeSave
+{
+
+    private const FIELDS_TO_WATCH = [
+        'cTeam',
+    ];
+
+    public function __construct(
+        private readonly EntityManager $entityManager,
+        private readonly GroupAssignmentService $groupAssignmentService,
+    ) {}
+
+    /**
+     * @inheritDoc
+     */
+    public function beforeSave(Entity $entity, SaveOptions $options): void
+    {
+        $person = $this->entityManager->getEntityById($entity->get('parentType'), $entity->get('parentId'));
+        try {
+            $this->groupAssignmentService->syncGroupsFromFields($entity, self::FIELDS_TO_WATCH, $person);
+        } catch (\Exception $e) {
+            $GLOBALS['log' ]->error('Meeting Before Save Hook error: ' . $e->getMessage());
+        }
+    }
+}
